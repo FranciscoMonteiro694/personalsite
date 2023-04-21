23569f7447574f1a4e8c57e0451c044ef9ffbcbd
Francisco Monteiro <fmonteiro@sentilant.com>
2023-04-21 22:47:34 +0100

    Initial setup and cleaning of the repo

---
+++ personal-website/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+type HeaderProps = {
+  title: string;
+};
+
+const Header = () => {
+  return <div className="main-div">Header</div>;
+};
+
+export default Header;
