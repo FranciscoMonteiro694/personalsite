67922a8cd20ff44a73369d3bc34622299ceaea7f
Francisco Monteiro <fmonteiro@sentilant.com>
2023-05-12 23:09:06 +0100

    Add nav to header component

--- personal-website/src/components/Header.tsx
+++ personal-website/src/components/Header.tsx
@@ -6,7 +6,22 @@ type HeaderProps = {
 };
 
 const Header = () => {
-  return <div className="main-div">Header</div>;
+  return (
+    <div className="main-div">
+      <div className="left-container">
+        <a>Francisco</a>
+      </div>
+      <div className="right-container">
+        <nav>
+          <a className="a-button">Home</a>
+          <a className="a-button">About</a>
+          <a className="a-button">Experience</a>
+          <a className="a-button">Education</a>
+          <a className="a-button">Contact</a>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
